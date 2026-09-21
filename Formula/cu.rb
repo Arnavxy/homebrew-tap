ff4d982974687cc04cb940887fb4671b331068f8
Arnav Khairate <arnav@Arnavs-MacBook-Pro.local>
2026-09-21 18:18:31 +0530

    Update cu to 0.2.7

--- Formula/cu.rb
+++ Formula/cu.rb
@@ -1,8 +1,8 @@
 class Cu < Formula
   desc "Token-aware computer-use runtime for macOS agents"
   homepage "https://github.com/Arnavxy/cu"
-  url "https://github.com/Arnavxy/cu/archive/refs/tags/v0.2.6.tar.gz"
-  sha256 "ae3100f9661cdd5faed8f74e62a6ec3bf4e488c4b2b902a63e3a8785c4c592c5"
+  url "https://github.com/Arnavxy/cu/archive/refs/tags/v0.2.7.tar.gz"
+  sha256 "54e6b4db07c1a061d909176ab922af9596daff47b0fe7349a821aca2ce948374"
   license "MIT"
 
   depends_on "cliclick"
@@ -21,7 +21,7 @@ class Cu < Formula
   end
 
   test do
-    assert_equal "cu 0.2.6", shell_output("#{bin}/cu --version").strip
+    assert_equal "cu 0.2.7", shell_output("#{bin}/cu --version").strip
 
     (testpath/"pixel.svg").write <<~SVG
       <svg xmlns="http://www.w3.org/2000/svg" width="2" height="2">
